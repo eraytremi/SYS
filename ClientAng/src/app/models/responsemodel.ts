02849ad8c09bbd3c7d0c88ae89a4c2e093d28b06
Eray Türemiş <85448685+eraytremi@users.noreply.github.com>
2024-09-18 13:39:26 +0300

    .

--- ClientAng/src/app/models/responsemodel.ts
+++
@@ -1,4 +0,0 @@
-export class ResultModel<T> {
-    data?: T;
-    errorMessages?: string[];
-  }
--- ClientAng/src/app/models/usermodel.ts
+++
@@ -1,6 +0,0 @@
-export class UserModel {
-    id:number=0;
-    name: string = "";
-    mail: string = "";
-  }
-  
